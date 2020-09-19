68444a75bd43e92af1aef7759db978c175418d12
diegolarraz <diegolarraz1@gmail.com>
2020-09-19 12:41:50 +0100

    savingchanges

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+function getButtonText() {
+  return 'Click on Me!'
+}
+
 // create a react component
 const App = function() {
-  return <div>Hi there!</div>;
+  const buttonText = 'Click Me!';
+  return (
+  <div>
+    <label className="label" htmlFor="name">Enter name:</label>
+    <input id="name" type="text" />
+    <button style={{ backgroundColor: 'blue', color: 'white', marginLeft: '20px' }}>{getButtonText()}</button>
+  </div>
+  );
 };
 
 ReactDom.render(
